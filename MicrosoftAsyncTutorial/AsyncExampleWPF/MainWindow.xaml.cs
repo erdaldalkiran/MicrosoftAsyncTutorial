5fe650aa3839b7d8c248f2aa12241bfc012d8b05
erdalkiran <erdalkiran@gmail.com>
2014-04-16 16:49:51 +0300

    Unit1

--- MicrosoftAsyncTutorial/AsyncExampleWPF/MainWindow.xaml.cs
+++ MicrosoftAsyncTutorial/AsyncExampleWPF/MainWindow.xaml.cs
@@ -1,20 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AsyncExampleWPF
 {
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Windows.Documents;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +17,74 @@ namespace AsyncExampleWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = false;
+            ResultsTextBox.Clear();
+            await SumPageSizesAsync();
+            ResultsTextBox.Text += "\r\nControl returned to StartButton_Click";
+            StartButton.IsEnabled = true;
+        }
+
+        private async Task SumPageSizesAsync()
+        {
+            var total = 0;
+            foreach (var url in SetUpURLList())
+            {
+                var client = new HttpClient { MaxResponseContentBufferSize = 1000000 };
+                var content = await client.GetByteArrayAsync(url);
+                DisplayResults(
+                    url,
+                    content
+                );
+                total += content.Length;
+            }
+
+            ResultsTextBox.Text += string.Format("\r\n\r\nTotal bytes returned: {0}\r\n", total);
+        }
+
+        private void DisplayResults(string url, byte[] content)
+        {
+            ResultsTextBox.Text += string.Format("\n{0,-58} {1,8}",
+                                                    url.Replace("http://", ""),
+                                                    content.Length
+                                                );
+        }
+
+        private async Task<byte[]> GetUrlContentsAsync(string url)
+        {
+            var content = new MemoryStream();
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+
+            using (var response = await request.GetResponseAsync())
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    await responseStream.CopyToAsync(content);
+                }
+            }
+
+            return content.ToArray();
+        }
+
+        private List<string> SetUpURLList()
+        {
+            return new List<string> 
+    { 
+        "http://msdn.microsoft.com/library/windows/apps/br211380.aspx",
+        "http://msdn.microsoft.com",
+        "http://msdn.microsoft.com/en-us/library/hh290136.aspx",
+        "http://msdn.microsoft.com/en-us/library/ee256749.aspx",
+        "http://msdn.microsoft.com/en-us/library/hh290138.aspx",
+        "http://msdn.microsoft.com/en-us/library/hh290140.aspx",
+        "http://msdn.microsoft.com/en-us/library/dd470362.aspx",
+        "http://msdn.microsoft.com/en-us/library/aa578028.aspx",
+        "http://msdn.microsoft.com/en-us/library/ms404677.aspx",
+        "http://msdn.microsoft.com/en-us/library/ff730837.aspx"
+    };
         }
     }
 }
